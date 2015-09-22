72a4aaf569d9f11273271e6d0456e5c1ee8cd3cc
Muhammad5253 <mhmd_virgo@hotmail.com>
2015-09-22 08:29:53 -0700

    Create Main.java

---
+++ lab-task-04/Main.java
@@ -0,0 +1,18 @@
+
+public class Main {
+
+	public static void main(String[] args) 
+	{
+
+		//ClassA objA = new ClassA();
+		//ClassA objB = new ClassB();
+		ClassC objC = new ClassC();
+		
+		//objC.printALine();
+		int res;
+		res = objC.Multiply(3, 4);
+		System.out.println(res);
+		System.out.println(objC.printHello());
+	}
+
+}
