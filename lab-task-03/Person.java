9e94b11efce617769dbd828b71f0d1e94b74997c
Muhammad5253 <mhmd_virgo@hotmail.com>
2015-09-22 08:24:31 -0700

    Create Person.java

---
+++ lab-task-03/Person.java
@@ -0,0 +1,35 @@
+
+public class Person 
+{
+	
+	public String Name; 
+	public int Age, Height;
+
+	public void Speak()
+	{
+		
+	}
+	
+	public void Listen()
+	{
+		
+	}
+	
+	public void Eat()
+	{
+		
+	}
+	
+	public void Run()
+	{
+		
+	}
+	
+	public void Walk()
+	{
+		
+	}
+
+
+
+}
