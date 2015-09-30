753d43e258d8cc063210079fd00917910a20277d
Muhammad5253 <mhmd_virgo@hotmail.com>
2015-09-30 07:03:42 -0700

    Create Szabist.java

---
+++ University-Task/Szabist.java
@@ -0,0 +1,19 @@
+package Uni;
+
+public class Szabist extends University
+{
+
+	protected String depName;
+	
+	/*public Szabist()
+	{
+		super("Szabist");
+	}*/
+	
+	public Szabist(String Name)
+	{
+		super("Szabist");
+		depName = Name;
+	}
+	
+}
