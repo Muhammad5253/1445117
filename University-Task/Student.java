41182a8915373c6bae352cf5c8a139e78097def5
Muhammad5253 <mhmd_virgo@hotmail.com>
2015-09-30 07:03:16 -0700

    Create Student.java

---
+++ University-Task/Student.java
@@ -0,0 +1,16 @@
+package Uni;
+
+public class Student extends Department
+{
+	
+	protected String studentName;
+	protected int studentId;
+	public Student(String Name, int Id, String deptName)
+	{
+		
+		super(deptName);
+		studentName = Name;
+		studentId = Id;
+	}
+
+}
